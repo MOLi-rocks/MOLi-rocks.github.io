d48a5ffa89626af7eb8576d4d28898a3d702282b
Jerry Wang <focaaby@gmail.com>
2015-12-04 12:51:45 +0800

    reindent

--- js/style.js
+++ js/style.js
@@ -16,8 +16,8 @@ var eventlist = {
 			}
 		});
 	},
-	compareDate: function(list){
-		for(var i = 0; i < list.length; i++){
+	compareDate: function(list) {
+		for(var i = 0; i < list.length; i++) {
 			var eventDate = Date.parse((list[i].content).substring(3, 13));
 			var currDate = Date.parse((new Date()).toDateString());
 			if(eventDate > currDate)
@@ -27,9 +27,9 @@ var eventlist = {
 		}
 	},
 	setEvents: function(title, summary, url) {
-		console.log(title + summary +url);
-		this.target.append('<h2>' + title+'</h2>');
-		this.target.append('<p>' + summary+'</p>');
+		console.log(title + summary + url);
+		this.target.append('<h2>' + title + '</h2>');
+		this.target.append('<p>' + summary + '</p>');
 		this.target.append('<a href="' + url + '"><button class="ui positive button fluid">立刻報名！</button></a>')
-	},
+	}
 };
