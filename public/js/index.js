2f9e25493954d57c2f6b78149dd96d459969dec2
Yuer <passerbyid@gmail.com>
2017-01-23 22:04:51 +0800

    首頁增加顯示最新活動資訊。

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+setDoorStatus();
+setLatestEvent();
+setMap();
+
+/*
+** 設定最新活動資訊
+*/
+function setLatestEvent() {
+  getNewEvents()
+    .done(function(events = []) {
+      if (events.length > 0) {
+        var latestEvent = events[0];
+
+        $('.latest-event-title').html(latestEvent.title);
+        $('.latest-event-date').html(latestEvent.dateString);
+        $('.latest-event-summary').html(latestEvent.summary);
+        $('.latest-event-link').attr('href', latestEvent.url);
+        $('.latest-event').show();
+      }
+    })
+    .fail(function(err) {
+      console.log(err);
+    });
+}
--- public/main.js
+++ public/main.js
@@ -1,6 +1,58 @@
 'use strict';
 
 /*
+** 取得如「2017/01/05（週四） 18:10」格式的時間字串
+*/
+function getLocaleDateString(date) {
+  var options = {
+    hour12: false,
+    weekday: 'short',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit'
+  };
+  return date.toLocaleString('zh-TW', options);
+}
+
+/*
+** 取得活動資訊
+*/
+function getEvents() {
+  return $.ajax('https://moli.rocks/kktix/events.json', {
+    method: 'GET'
+  }).then(function(data){
+    return data.entry;
+  });
+}
+
+/*
+** 取得未發生的活動資訊
+*/
+function getNewEvents() {
+  return getEvents().then(function(events = null) {
+    if (events == null) {
+      return events;
+    }
+
+    var newEvents = [];
+
+    // 過濾出未發生之活動
+    events.forEach(function(event){
+      var date = new Date(event.published);
+      if ( date.getTime() >= new Date() ) {
+        // 格式化活動時間
+        event.dateString = getLocaleDateString( new Date(event.published) );
+        newEvents.push(event);
+      }
+    });
+
+    return newEvents;
+  });
+}
+
+/*
 ** 偵測開門狀態
 */
 function setDoorStatus() {
