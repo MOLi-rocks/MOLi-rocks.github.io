19ff246d0662dc9344bc6ea652ed4f6d7e63fa1f
Yuer <passerbyid@gmail.com>
2017-01-25 23:11:26 +0800

    嘗試做個自動部屬。

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BRANCH=${1}
+
+echo "Start deployment"
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout $BRANCH
+echo "building..."
+npm install
+npm run build
+echo "Finished."
---
+++ index.js
@@ -0,0 +1,31 @@
+require('./env.js');
+var http = require('http');
+var createHandler = require('github-webhook-handler');
+var handler = createHandler({ path: '/webhook', secret: proces.env.SERCERT_KEY });
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('no such location');
+  })
+}).listen(process.env.PORT || 3000);
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message);
+});
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref);
+  run_cmd('sh', ['./build.sh', process.env.BRANCH], function(text) { console.log(text) });
+});
+
+function run_cmd(cmd, args, callback) {
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
+
+  child.stdout.on('data', function(buffer) { resp += buffer.toString(); });
+  child.stdout.on('end', function() { callback (resp) });
+}
