e1eb84cecec598a1c42c8257e692e603a0761bc6
richegg Tsai <zxp86021@users.noreply.github.com>
2019-06-19 10:07:33 +0800

    該畢業的畢業

--- app/keyholder/keyholder.controller.js
+++ app/keyholder/keyholder.controller.js
@@ -27,31 +27,6 @@
         "username": "Trustmyself79"
       },
       {
-        "id": 13043549,
-        "name": "保保",
-        "username": "Bilthe"
-      },
-      {
-        "id": 13043539,
-        "name": "nhss",
-        "username": "bf98099"
-      },
-      {
-        "id": 8233294,
-        "name": "和融",
-        "username": "sunnyworm"
-      },
-      {
-        "id": 8147952,
-        "name": "ChaNg Y.y.",
-        "username": "x3388638"
-      },
-      {
-        "id": 12278450,
-        "name": "小青",
-        "username": "SEMI02"
-      },
-      {
         "id": 17447427,
         "name": "奕良",
         "username": "YiLiangChen"
